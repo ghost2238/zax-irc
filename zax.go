be7a9083eedb7f2b6af2fef60575ec47be9e7039
Ghost <ghsthack@gmail.com>
2016-01-13 23:18:32 +0100

    More random stuff

--- zax.go
+++ zax.go
@@ -7,11 +7,13 @@ import (
 	"github.com/mvdan/xurls"
 	"github.com/yhat/scrape"
 	"golang.org/x/net/html"
+	"math/rand"
 	"net/http"
 	"os"
 	"regexp"
+	"strconv"
 	"strings"
-	//"time"
+	"time"
 )
 
 type Channel struct {
@@ -19,19 +21,11 @@ type Channel struct {
 	Password string
 }
 
-/*type Message struct {
-	Timestamp time
-	Text      string
-}*/
-
 type User struct {
 	Nicknames []string
 	Clients   []string
 	//Messages  []Message
 	Urls []string
-}
-
-type Reddit struct {
 }
 
 type Config struct {
@@ -45,6 +39,49 @@ type Config struct {
 	News       []string
 }
 
+func steam_search_request(url string) (string, bool) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return "", false
+	}
+	root, err := html.Parse(resp.Body)
+	if err != nil {
+		return "", false
+	}
+
+	matcher := func(n *html.Node) bool {
+		return scrape.Attr(n, "class") == "responsive_search_name_combined"
+	}
+
+	games := scrape.FindAll(root, matcher)
+	if games == nil || len(games) == 0 {
+		return "", false
+	}
+	game := games[0]
+	err_ := false
+	title, err_title := scrape.Find(game, func(n *html.Node) bool { return scrape.Attr(n, "class") == "title" })
+	href := game.Parent
+	_, err_ = scrape.Find(game, func(n *html.Node) bool { return scrape.Attr(n, "class") == "platform_img win" })
+	s_win := ""
+	s_mac := ""
+	s_linux := ""
+	if err_ == true {
+		s_win = "Win"
+	}
+	_, err_ = scrape.Find(game, func(n *html.Node) bool { return scrape.Attr(n, "class") == "platform_img mac" })
+	if err_ == true {
+		s_mac = "/Mac"
+	}
+	_, err_ = scrape.Find(game, func(n *html.Node) bool { return scrape.Attr(n, "class") == "platform_img linux" })
+	if err_ == true {
+		s_linux = "/Linux"
+	}
+	if err_title == true {
+		return fmt.Sprintf("[Steam Release] %s - %s [%s%s%s]\n", scrape.Text(title), scrape.Attr(href, "href"), s_win, s_mac, s_linux), true
+	}
+	return "", false
+}
+
 func search_reddit(url string) (string, bool) {
 	resp, err := http.Get("https://www.reddit.com/search?q=url%3A" + url + "&sort=relevance&t=all")
 	if err != nil {
@@ -54,7 +91,7 @@ func search_reddit(url string) (string, bool) {
 	if err != nil {
 		return "", false
 	}
-	// define a matcher
+
 	matcher := func(n *html.Node) bool {
 		return scrape.Attr(n, "class") == "search-title may-blank"
 	}
@@ -90,6 +127,37 @@ func search_reddit(url string) (string, bool) {
 	return s_final, true
 }
 
+func rand_int(min, max int) int {
+	rand.Seed(time.Now().Unix())
+	return rand.Intn(max-min) + min
+}
+
+func is_command(text string, cmds []string) bool {
+	for _, cmd := range cmds {
+		if strings.HasPrefix(text, cmd) {
+			return true
+		}
+	}
+	return false
+}
+
+func get_quit_msg() string {
+
+	switch rand_int(0, 4) {
+	case 0:
+		return "Uh, never mind."
+	case 1:
+		return "This system is too advanced for you."
+	case 2:
+		return "That was an illogical decision."
+	case 3:
+		return "Weeeeeeeeeeeeeeeeeeeeee[bzzt]"
+	case 4:
+		return "Didn't we have some fun, though?"
+	}
+	return "Interesting."
+}
+
 func main() {
 	fmt.Println("Loading config...")
 
@@ -109,6 +177,9 @@ func main() {
 	cfg.NewNick = func(n string) string { return n + "^" }
 	c := irc.Client(cfg)
 
+	quit := make(chan bool)
+	last_url := ""
+
 	c.HandleFunc(irc.CONNECTED,
 		func(conn *irc.Conn, line *irc.Line) {
 			for i := 0; i < len(config.Channels); i++ {
@@ -117,22 +188,37 @@ func main() {
 			}
 		})
 
-	quit := make(chan bool)
 	c.HandleFunc(irc.DISCONNECTED,
-		func(conn *irc.Conn, line *irc.Line) { quit <- true })
+		func(conn *irc.Conn, line *irc.Line) {
+			fmt.Println("Disconnected")
+			quit <- true
+		})
 
-	/*c.HandleFunc(irc.PING,
-	func(conn *irc.Conn, line *irc.Line) {
+	c.HandleFunc(irc.JOIN,
+		func(conn *irc.Conn, line *irc.Line) {
+			fmt.Println("[" + line.Target() + "] " + line.Nick + " (~" + line.Ident + "@" + line.Host + ") has joined.")
 
-	})*/
+			if line.Nick == config.Nickname {
+				return
+			}
+		})
+
+	c.HandleFunc(irc.QUIT,
+		func(conn *irc.Conn, line *irc.Line) {
+			fmt.Println("[" + line.Target() + "] " + line.Nick + " (" + line.Ident + "@" + line.Host + ") has quit.")
+			time.Sleep(100 * time.Millisecond)
+		})
+
+	c.HandleFunc(irc.PING,
+		func(conn *irc.Conn, line *irc.Line) {
+			fmt.Println("PING.")
+			time.Sleep(100 * time.Millisecond)
+		})
 
 	c.HandleFunc(irc.PRIVMSG,
 		func(conn *irc.Conn, line *irc.Line) {
-
-			p := strings.Split(line.Src, "!")
-			sender := p[0]
+			sender := line.Nick
 			text := line.Text()
-
 			fmt.Println("[" + line.Target() + "] " + sender + ": " + text)
 
 			if len(config.ReportChan) > 0 {
@@ -140,22 +226,85 @@ func main() {
 			}
 
 			if sender == config.Admin {
-				if line.Text() == "die" {
-					c.Quit("As you say.")
+				if line.Text() == "!" || line.Text() == "<<" {
+					c.Quit(get_quit_msg())
 				}
 			}
+			cmd_rand := []string{".r", ".random"}
+			cmd_steam := []string{".s", ".steam"}
+			args := strings.Split(text, " ")
+
+			if is_command(text, cmd_rand) {
+				if len(args) < 3 {
+					return
+				}
+				min, err := strconv.ParseInt(args[1], 10, 32)
+				if err != nil {
+					fmt.Println("Failed to parse min.")
+					return
+				}
+				max, err := strconv.ParseInt(args[2], 10, 32)
+				if err != nil {
+					fmt.Println("Failed to parse max.")
+					return
+				}
+
+				c.Privmsg(line.Target(), "What about... "+strconv.Itoa(rand_int(int(min), int(max))))
+			}
+
+			if is_command(text, cmd_steam) {
+				if len(args) < 2 {
+					return
+				}
+				if args[1] == "latest" {
+					result, success := steam_search_request("http://store.steampowered.com/search/?sort_by=Released_DESC&tags=-1&category1=998&page=1")
+					if success {
+						c.Privmsg(line.Target(), result)
+						fmt.Println(result)
+					} else {
+						fmt.Println("Failed to retrieve steam search result.")
+					}
+				}
+				if args[1] == "find" {
+					re := regexp.MustCompile("steam search ([[:alnum:] ]+)")
+					match := re.FindStringSubmatch(text)
+					if match == nil || len(match) == 0 {
+						fmt.Println("Doesn't match.")
+						return
+					}
+					search_url := "http://store.steampowered.com/search/?snr=&term=" + match[1]
+					fmt.Println("Search URL: " + search_url)
+					result, success := steam_search_request(search_url)
+					if success {
+						c.Privmsg(line.Target(), result)
+					} else {
+						fmt.Println("Failed to retrieve steam search result.")
+					}
+				}
+				return
+			}
+
+			// Handle URLs
 			if !(sender == "Wipe" && (strings.Contains(text, "Steam") || strings.Contains(text, "YouTube"))) {
 				urls := xurls.Relaxed.FindAllString(text, -1)
 				for i := 0; i < len(urls); i++ {
-					reddit, success := search_reddit(urls[i])
+					url := urls[i]
+					fmt.Println("Found url " + url)
+					if url == last_url {
+						fmt.Println("Matches same url as last time, ignore.")
+						continue
+					}
+					reddit, success := search_reddit(url)
 					if success {
 						c.Privmsg(line.Target(), reddit)
 					} else {
-						fmt.Println("Failed to retrieve reddit URL.")
+						fmt.Println("Failed to retrieve reddit URL for the link.")
 					}
+					last_url = url
 				}
 			}
 
+			time.Sleep(100 * time.Millisecond)
 		})
 
 	if err := c.Connect(); err != nil {
